712aff120386dd6614db0a14cf1cb6c55f24c4e9
MuratG <muratgozderesi34@gmail.com>
2021-01-16 17:17:29 +0300

    initial

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,41 @@
-import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <div className={styles.content}>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <div className={styles.containerHeader}>
+          <p className={styles.headerTitle}>New Title</p>
         </div>
-      </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+        <div className={styles.main}>
+
+          <input type="text" placeHolder="New Title" className={styles.mainInput} name="Title" />
+
+          <div className={styles.mainDesc}>
+            <textarea
+              name="comments"
+              placeholder="New Description"
+              className={styles.mainDescInput}
+              maxlength="1375">
+            </textarea>
+          </div>
+
+          <div className={styles.mainImage}>
+            <p className={styles.plus}>+</p>
+            <p className={styles.imageText}>GÖRSEL</p>
+          </div>
+
+          <button className={styles.button}></button>
+
+        </div>
+
+      </div>
+
     </div>
   )
 }
+{/* <p className={styles.mainDescText}>New Description</p> */ }
+{/* <input type="text" placeHolder="New Description"  name="desc" /> */ }
