f061b8d6ef3204daae6a255a2a218024666a9c69
MuratG <muratgozderesi34@gmail.com>
2021-01-17 02:53:45 +0300

    upload image

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,22 @@
 import styles from '../styles/Home.module.css'
-
+import { useState } from 'react'
 export default function Home() {
+
+const [state, setState] = useState( {file: "",imagePreviewUrl: ""})
+
+function onChange(e) {
+  let file = e.target.files;
+  let reader = new FileReader();
+
+  reader.onloadend = () => {
+    setState({
+      file : file,
+      imagePreviewUrl : reader.result
+      })
+  }
+    reader.readAsDataURL(file[0]);
+}  
+  let {imagePreviewUrl} = state;
   return (
     <div className={styles.container}>
 
@@ -12,20 +28,39 @@ export default function Home() {
 
         <div className={styles.main}>
 
-          <input type="text" placeHolder="New Title" className={styles.mainInput} name="Title" />
+          <input type="text" placeholder="New Title" className={styles.mainInput} name="Title" />
 
           <div className={styles.mainDesc}>
             <textarea
               name="comments"
               placeholder="New Description"
               className={styles.mainDescInput}
-              maxlength="1375">
+              maxLength="1375">
             </textarea>
           </div>
 
           <div className={styles.mainImage}>
-            <p className={styles.plus}>+</p>
-            <p className={styles.imageText}>GÖRSEL</p>
+            <div className={styles.imagePlus} id="my-content"> 
+              <img
+                src= {imagePreviewUrl}
+                alt="imagenone"
+                className={  
+                  imagePreviewUrl ? 
+                  styles.uploadImage :
+                  styles.uplodaNone
+                }        
+              />
+              
+              <input type="file" 
+                id="myfile" name="myfile" 
+                className={imagePreviewUrl ? styles.plusButtonNone : styles.plusButton}  
+                onChange={(e) => onChange(e) }
+              />
+              
+              {/* <button className={styles.plusButton}> <span className={styles.plusSpan}>+</span> </button>
+              <p className={styles.imageText}>GÖRSEL</p> */}
+
+            </div>
           </div>
 
           <button className={styles.button}></button>
@@ -37,5 +72,13 @@ export default function Home() {
     </div>
   )
 }
-{/* <p className={styles.mainDescText}>New Description</p> */ }
-{/* <input type="text" placeHolder="New Description"  name="desc" /> */ }
+
+
+
+
+// // {/* <form enctype="multipart/form-data" action="/upload/image" method="post">
+//               // <input id="image-file" type="file" />
+//           // </form> */}
+
+// {/* <p className={styles.mainDescText}>New Description</p> */ }
+// {/* <input type="text" placeHolder="New Description"  name="desc" /}
