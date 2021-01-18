fa09c39889d3911bb52818a5c7f8fda2489e8ed1
MuratG <muratgozderesi34@gmail.com>
2021-01-18 03:33:51 +0300

    added style and fixed page

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ export default function Home() {
 const [state, setState] = useState( {file: "",imagePreviewUrl: ""})
 const [comment, setComment] = useState("")
 const [title, setTitle] = useState("")
+const [save,setSave] = useState({ saveTitle : "", saveDesc:"", saveImage: "" })
 
 function onWrite(e) {
   let file = e.target.files;
@@ -19,80 +20,131 @@ function onWrite(e) {
     reader.readAsDataURL(file[0]);
 }  
   
-  let {imagePreviewUrl} = state;
-  return (
+let {imagePreviewUrl} = state;
+
+function showPage() {
+  if(comment && title && imagePreviewUrl) {
+
+  setSave({ saveTitle : title, saveDesc: comment, saveImage: imagePreviewUrl})
+  setComment("")
+  setTitle("")
+  setState({ file:"" , imagePreviewUrl:""})
+  }
+}
+
+return (
     <div className={styles.container}>
+        <div className={styles.content}>
+            <div className={styles.containerHeader}>
+              <p className={styles.headerTitle}>New Title</p>
+            </div>
+          
 
-      <div className={styles.content}>
+            <div className={styles.main}>
+            <input 
+              type="text" 
+              placeholder="New Title" 
+              className={styles.mainInput} 
+              name="Title" 
+              onChange={(e) => setTitle(e.target.value)} 
+              autocomplete="off"
+              value={title}
+            />
 
-        <div className={styles.containerHeader}>
-          <p className={styles.headerTitle}>New Title</p>
-        </div>
+            <div className={styles.mainDesc}>
+              <textarea
+                name="comments"
+                placeholder="New Description"
+                className={styles.mainDescInput}
+                maxLength="1460"
+                onChange={(e) => setComment(e.target.value)} 
+                value={comment}
+              >
+              </textarea>
+            </div>
 
-        <div className={styles.main}>
+            <div className={styles.mainImage}>
+              <div className={styles.imagePlus} id="my-content"> 
+                <img
+                  src= {imagePreviewUrl}
+                  alt="imagenone"
+                  className={  
+                    imagePreviewUrl ? 
+                    styles.uploadImage :
+                    styles.uploadNone
+                  }        
+                />
+                <label for="myfile" >
+                  <span className={imagePreviewUrl ? styles.plusButton : styles.plusSpan}>+</span>
+                </label>
+                <input 
+                  type="file" 
+                  id="myfile" 
+                  name="myfile" 
+                  className={styles.plusFile}  
+                  onChange={(e) => onWrite(e)}
+                />
+                
+                <p className={imagePreviewUrl ? styles.imageNoneText : styles.imageText}>GÖRSEL</p> 
 
-          <input 
-            type="text" 
-            placeholder="New Title" 
-            className={styles.mainInput} 
-            name="Title" 
-            onChange={(e) => setTitle(e.target.value)} 
-          />
-
-          <div className={styles.mainDesc}>
-            <textarea
-              name="comments"
-              placeholder="New Description"
-              className={styles.mainDescInput}
-              maxLength="1460"
-              onChange={(e) => setComment(e.target.value)} 
-            >
-            </textarea>
-          </div>
-
-          <div className={styles.mainImage}>
-            <div className={styles.imagePlus} id="my-content"> 
-              <img
-                src= {imagePreviewUrl}
-                alt="imagenone"
-                className={  
-                  imagePreviewUrl ? 
-                  styles.uploadImage :
-                  styles.uplodaNone
-                }        
-              />
-              <label for="myfile" >
-                <span className={imagePreviewUrl ? styles.plusButton : styles.plusSpan}>+</span>
-              </label>
-              <input 
-                type="file" 
-                id="myfile" 
-                name="myfile" 
-                className={styles.plusFile}  
-                onChange={(e) => onWrite(e)}
-              />
-              
-              <p className={imagePreviewUrl ? styles.imageNoneText : styles.imageText}>GÖRSEL</p> 
+              </div>
 
             </div>
+            <button 
+              className={comment && title && imagePreviewUrl ? styles.buttonActive : styles.button}
+              onClick={showPage}
+            >
+            </button>
+
           </div>
 
-          <button className={styles.button}></button>
-
         </div>
+        
+        <div> <hr className={styles.line}/> </div>
+                    
+      {/* ////////////////////////////////////////////////////////////////////////////////// */}
+        <div className={styles.content2}>
+          <div className={styles.containerHeader}>
+            <p className={styles.headerTitle}>New Title</p>
+          </div>
+    
+          <div className={styles.main}>
 
-      </div>
+            <input 
+              type="text" 
+              className={styles.mainInput} 
+              name="Title" 
+              value={save.saveTitle}
+              readOnly
+            />
 
+            <div className={styles.mainDesc}>
+              <textarea
+                name="comments"
+                className={styles.mainDescInput}
+                maxLength="1460"
+                value={save.saveDesc}
+                readOnly
+              >
+              </textarea>
+            </div>
+
+            <div className={styles.mainDone}>
+              <div className={styles.imagePlus} id="my-content"> 
+                <img
+                  src= {save.saveImage}
+                  alt="imagenone"
+                  className={
+                    save.saveImage ? 
+                    styles.uploadImage :
+                    styles.uploadNone
+                  }        
+                />
+              </div>
+            </div>
+
+          </div>
+        </div>
     </div>
   )
 }
-
-
-
-
-// // {/* <form enctype="multipart/form-data" action="/upload/image" method="post">
-//               // <input id="image-file" type="file" />
-//           // </form> */}
-
-// {/* <p className={styles.mainDescText}>New Description</p> */ }
-// {/* <input type="text" placeHolder="New Description"  name="desc" /}
